13ef86ccce624c0139c5b3ee7e80c9fefce8474e
Rbowie100 <Ryan.Bowie321@gmail.com>
2025-12-08 05:03:21 -0500

    Update time.java

--- time.java
+++ time.java
@@ -3,25 +3,36 @@ public class time {
     int hour=6;
     String Time1="A.M";
     String Time2="P.M";
+    //checks whether it is AM
     boolean AM=true;
+    //Checks whether it is PM
     boolean PM=false;
+    //end of day
     int timeend=10;
+    //start of day
     int timestart=6;
     String[] Days={"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+    //holds the averages for each day
     double [] dayvalues=new double[7];
+    //holds the max for each day
     double [] max=new double[7];
+    //holds the min for each day
     double [] min=new double[7];
+    //holds the standard deviation for each day
     double [] standarddeviation=new double[7];
+    //holds the confidence interval for each day
     double [] confidenceintervalplus= new double [7];
     double [] confidenceintervalminus= new double [7];
 
-
+    //returns the arrival time hour
     public int Arrivaltimehour (){
         return hour;
     }
+    //returns the arrival time minute
     public int Arrivaltimeminute (){
         return minute;
     }
+    //returns whether it is AM or PM
     public String Arrival(){
         if(AM){
             return Time1;
@@ -31,6 +42,7 @@ public class time {
         }
         return "AM";
     }
+    //increase the time of the clock
 
     public void increasetime(){
       minute+=1;
@@ -47,7 +59,7 @@ public class time {
       }
 
     }
-
+//reset time back to the beginning
     public void resettime(){
         hour=6;
         minute=0;
