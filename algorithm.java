d56413b49b57a0dc3bff1fa2b2117401616acb22
Rbowie100 <Ryan.Bowie321@gmail.com>
2025-12-08 05:02:26 -0500

    Update algorithm.java

--- algorithm.java
+++ algorithm.java
@@ -11,7 +11,7 @@ public class algorithm {
         Averagetimespent=Averagetime;
 
     }
-
+// Poisson algorithm for five minute increments returns a probability
     public double Poissond5mins(){
         int historical=historicalCustomer/12;
         double factor=historical;
@@ -29,6 +29,7 @@ public class algorithm {
         go= value.doubleValue();
         return go;
     }
+    // Poisson algorithm for ten minute increments returns a probability
     public double Poissond10mins(){
         int historical=historicalCustomer/6;
         double factor=historical;
@@ -48,6 +49,7 @@ public class algorithm {
 
         return go;
     }
+    // Poisson algorithm for fifteen minute increments returns a probability
     public double Poissond15mins(){
         int historical=historicalCustomer/4;
         double factor=historical;
@@ -67,6 +69,8 @@ public class algorithm {
 
         return go;
     }
+
+    // Poisson algorithm for sixty minute increments returns a probability
     public double Poissond60mins(){
         int historical=historicalCustomer;
         double factor=historical;
@@ -80,12 +84,14 @@ public class algorithm {
         double go= ((Math.pow(historical,historical))*(Math.pow(Math.E,-historical)));
         BigDecimal value=BigDecimal.valueOf(go);
 
-       value=value.divide(whole,7, RoundingMode.HALF_DOWN);
-       go= value.doubleValue();
+        value=value.divide(whole,7, RoundingMode.HALF_DOWN);
+        go= value.doubleValue();
         //BigInteger[] value= whole.divideAndRemainder(num);
 
         return go;
     }
+
+    // Exponential algorithm for customers shopping returns a probability
     public double ExponenetialdistributionShop(double currenttime){
         double exponent=0;
         double value=0;
@@ -98,6 +104,8 @@ public class algorithm {
 
         return value;
     }
+
+    // Exponential algorithm for customers checkout returns a probability
     public double ExponenetialdistributionCheckout(double currenttime){
         double exponent=0;
         double value=0;
@@ -111,4 +119,5 @@ public class algorithm {
     }
 
 }
+
 
